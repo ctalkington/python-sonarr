643aecbee7bac9898d8c1ab644cb2cc8bde6245b
Chris Talkington <chris@talkingtontech.com>
2020-04-04 15:31:41 -0500

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""Tests for Sonarr."""
+import os
+
+
+def load_fixture(filename):
+    """Load a fixture."""
+    path = os.path.join(os.path.dirname(__file__), "fixtures", filename)
+    with open(path) as fptr:
+        return fptr.read()
