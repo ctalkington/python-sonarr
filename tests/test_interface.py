291e5d5e61b8cf052d0afd7e2a02c14a5530fc9c
Chris Talkington <chris@talkingtontech.com>
2020-04-04 16:18:02 -0500

    Create test_interface.py

---
+++ tests/test_interface.py
@@ -0,0 +1,55 @@
+"""Tests for Sonarr."""
+from typing import List
+
+import pytest
+from aiohttp import ClientSession
+from sonarr import Sonarr, SonarrError
+from sonarr.models import Info
+
+from . import load_fixture
+
+API_KEY = "MOCK_API_KEY"
+HOST = "192.168.1.89"
+PORT = 8989
+
+MATCH_HOST = f"{HOST}:{PORT}"
+
+
+@pytest.mark.asyncio
+async def test_update(aresponses):
+    """Test update is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/api/system/status",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("system-status.json"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/api/diskspace",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            text=load_fixture("diskspace.json"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        client = Sonarr(HOST, API_KEY, session=session)
+        response = await client.update()
+
+        assert response
+        assert isinstance(response.info, Info)
+        assert isinstance(response.disks, List)
+
+        response = await client.update()
+
+        assert response
+        assert isinstance(response.info, Info)
+        assert isinstance(response.disks, List)
