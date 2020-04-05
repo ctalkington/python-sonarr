9efe1969a9c7f571d3f4ecaa78aa3ddd62e8ac8b
Chris Talkington <chris@talkingtontech.com>
2020-04-04 20:04:01 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,5 @@
 """Tests for Sonarr Models."""
+from datetime import datetime, timezone
 import json
 
 import pytest
@@ -8,6 +9,7 @@ from sonarr import SonarrError
 from . import load_fixture
 
 INFO = json.loads(load_fixture("system-status.json"))
+CALENDAR = json.loads(load_fixture("calendar.json"))
 DISKSPACE = json.loads(load_fixture("diskspace.json"))
 APPLICATION = {"info": INFO, "diskspace": DISKSPACE}
 
@@ -37,7 +39,16 @@ def test_info() -> None:
     info = models.Info.from_dict(INFO)
 
     assert info
-    assert info.brand == "Sonarr"
+    assert info.app_name == "Sonarr"
+    assert info.version == "2.0.0.1121"
+
+
+def test_episode() -> None:
+    """Test the Episode model."""
+    info = models.Info.from_dict(CALENDAR[1])
+
+    assert info
+    assert info.app_name == "Sonarr"
     assert info.version == "2.0.0.1121"
 
 
@@ -50,3 +61,18 @@ def test_disk() -> None:
     assert disk.label == ""
     assert disk.free == 282500067328
     assert disk.total == 499738734592
+
+def test_series() -> None:
+    """Test the Series model."""
+    series = models.Series.from_dict(CALENDAR[1]["series"])
+
+    assert series
+    assert series.tvdb_id == 1
+    assert series.series_id == 1
+    assert series.slug == "bobs-burger"
+    assert series.title == "Bob's Burgers"
+    assert series.overview == "Bob's Burgers follows a third-generation restaurateur, Bob, as he runs Bob's Burgers with the help of his wife and their three kids. Bob and his quirky family have big ideas about burgers, but fall short on service and sophistication. Despite the greasy counters, lousy location and a dearth of customers, Bob and his family are determined to make Bob's Burgers \"grand re-re-re-opening\" a success."
+    assert series.network == "Fox"
+    assert series.runtime == 20
+    assert series.timeslot == "5:30pm"
+    assert series.premiered == datetime(2010, 7, 5, 15, 0, 8, tzinfo=timezone.utc)
