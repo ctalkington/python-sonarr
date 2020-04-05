902575c1af0cff389bccb868e4a5bdf4ca8c3d33
Chris Talkington <chris@talkingtontech.com>
2020-04-05 17:45:38 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,7 +104,7 @@ def test_queue_item() -> None:
 
 def test_season() -> None:
     """Test the Season model."""
-    season = models.Series.from_dict(SERIES[0]["seasons"][1])
+    season = models.Season.from_dict(SERIES[0]["seasons"][1])
  
     assert season
     assert season.number == 1
