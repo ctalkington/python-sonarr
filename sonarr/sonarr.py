3e341c51b8d81521d23c2c18707c1e119fe785f3
Chris Talkington <chris@talkingtontech.com>
2020-04-06 11:38:24 -0500

    Update sonarr.py

--- sonarr/sonarr.py
+++ sonarr/sonarr.py
@@ -10,7 +10,7 @@ from yarl import URL
 
 from .__version__ import __version__
 from .exceptions import SonarrAccessRestricted, SonarrConnectionError, SonarrError
-from .models import Application, Episode, QueueItem, SeriesItem, WantedResults
+from .models import Application, CommandItem, Episode, QueueItem, SeriesItem, WantedResults
 
 
 class Sonarr:
