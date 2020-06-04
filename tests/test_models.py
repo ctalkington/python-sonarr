1170189423abc9f05791c3f9e8f87c1bb805cc8b
Chris Talkington <chris@talkingtontech.com>
2020-06-03 19:49:14 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -40,6 +40,18 @@ def test_application_no_data() -> None:
         models.Application({})
 
 
+def test_dt_str_to_dt() -> None:
+    """Test the dt_str_to_dt method with long microseconds."""
+    dt = models.dt_str_to_dt("2018-05-14T19:02:13.101496Z")
+    assert dt == ""
+
+
+def test_dt_str_to_dt_long_microseconds() -> None:
+    """Test the dt_str_to_dt method with long microseconds."""
+    dt = models.dt_str_to_dt("2018-05-14T19:02:13.1014986Z")
+    assert dt == ""
+
+
 def test_info() -> None:
     """Test the Info model."""
     info = models.Info.from_dict(INFO)
