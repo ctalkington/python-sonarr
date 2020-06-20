4ff68f2b1517ded9266152a3a52a22786d97dd3c
Chris Talkington <chris@talkingtontech.com>
2020-06-20 15:48:51 -0500

    Update __init__.py

--- sonarr/__init__.py
+++ sonarr/__init__.py
@@ -1,8 +1,7 @@
 """Asynchronous Python client for Sonarr."""
-from .sonarr import (  # noqa
-    Client,
-    Sonarr,
+from .exceptions import (  # noqa
     SonarrAccessRestricted,
     SonarrConnectionError,
     SonarrError,
 )
+from .sonarr import Client, Sonarr  # noqa
